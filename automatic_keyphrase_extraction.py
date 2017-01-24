736d95625f4a2bd5fb713b7b20bc011971ccec58
morreene <morreene@gmail.com>
2017-01-24 23:37:47 +0100

    new commit

--- automatic_keyphrase_extraction.py
+++ automatic_keyphrase_extraction.py
@@ -71,4 +71,85 @@ def score_keyphrases_by_tfidf(texts, candidates='chunks'):
     
 b, c =score_keyphrases_by_tfidf(doc_complete)
 
+import pandas as pd
+
+
+for k, v in c.items():
+    print (k, v)
+
+for v in b:
+    print( v)
+    
+    print(b)
+
 texts = doc_complete
+
+
+
+import numpy as np
+import pandas as pd
+
+probMatrix = np.zeros(shape=(3,9))  # size of (num docs, k topics)
+
+for doc_num, probs in enumerate(b):
+    for k_index, prob in probs:
+        probMatrix[doc_num, k_index] = prob
+
+df_tfidf_matrix = pd.DataFrame(probMatrix)
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################
+def score_keyphrases_by_textrank(text, n_keywords=0.05):
+    from itertools import takewhile, tee, izip
+    import networkx, nltk
+    
+    # tokenize for all words, and extract *candidate* words
+    words = [word.lower()
+             for sent in nltk.sent_tokenize(text)
+             for word in nltk.word_tokenize(sent)]
+    candidates = extract_candidate_words(text)
+    # build graph, each node is a unique candidate
+    graph = networkx.Graph()
+    graph.add_nodes_from(set(candidates))
+    # iterate over word-pairs, add unweighted edges into graph
+    def pairwise(iterable):
+        """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
+        a, b = tee(iterable)
+        next(b, None)
+        return izip(a, b)
+    for w1, w2 in pairwise(candidates):
+        if w2:
+            graph.add_edge(*sorted([w1, w2]))
+    # score nodes using default pagerank algorithm, sort by score, keep top n_keywords
+    ranks = networkx.pagerank(graph)
+    if 0 < n_keywords < 1:
+        n_keywords = int(round(len(candidates) * n_keywords))
+    word_ranks = {word_rank[0]: word_rank[1]
+                  for word_rank in sorted(ranks.iteritems(), key=lambda x: x[1], reverse=True)[:n_keywords]}
+    keywords = set(word_ranks.keys())
+    # merge keywords into keyphrases
+    keyphrases = {}
+    j = 0
+    for i, word in enumerate(words):
+        if i < j:
+            continue
+        if word in keywords:
+            kp_words = list(takewhile(lambda x: x in keywords, words[i:i+10]))
+            avg_pagerank = sum(word_ranks[w] for w in kp_words) / float(len(kp_words))
+            keyphrases[' '.join(kp_words)] = avg_pagerank
+            # counter as hackish way to ensure merged keyphrases are non-overlapping
+            j = i + len(kp_words)
+    
+    return sorted(keyphrases.iteritems(), key=lambda x: x[1], reverse=True)
+
+score_keyphrases_by_textrank(text)
